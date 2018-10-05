3766e88d02876f72a7a0fca44ad74c4ca067f529
andreiyasinski <andrewtopol856@gmail.com>
2018-10-05 20:42:12 +0300

    :zap:

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.value = initialValue;
+    this.toString = () => eval(this.value);
   }
 
   add(number) {
-    // your implementation
+    this.value += ` +${number}`;
+    return this;
   }
-  
+
   subtract(number) {
-    // your implementation
+    this.value += ` -${number}`;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.value += ` *${number}`;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.value += ` /${number}`;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.value += ` **${number}`;
+    return this;
   }
 }
 
